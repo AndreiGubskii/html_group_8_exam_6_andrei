c32e42d306d22e3977fbd774a18c7706aba82bd7
AndreiGubskii <andrei.gubskii@gmail.com>
2017-04-22 13:09:12 +0600

    Добавил иконку для кнопки меню Добавил адаптивную менюшку Подключил Слайдер slick

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,25 @@
-console.log("hello")
+var btn_menu = $(".menu-btn"),
+menu = $(".menu");
+
+
+btn_menu.click(function () {
+	if (menu.is(":hidden")) {
+		menu.show();
+	} else {
+		menu.hide();		
+	}
+	return false;
+});
+
+$( window ).resize(function() {
+	if(document.body.clientWidth > 480){
+		menu.show();
+	}else{
+		menu.hide();
+	}
+});
+
+$(".slider").slick({
+	mobileFirst:true,
+	arrows : false
+});

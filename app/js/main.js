b7e620b7ed586f424ec9a3c8fabb84e47e3dcb28
AndreiGubskii <andrei.gubskii@gmail.com>
2017-04-22 20:58:32 +0600

    Добавил плавную прокрутку по якорям

--- app/js/main.js
+++ app/js/main.js
@@ -23,3 +23,18 @@ $(".slider").slick({
 	mobileFirst:true,
 	arrows : false
 });
+
+// Плавная прокрутка по якорям
+menu.on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 700 мс
+    $('body,html').animate({scrollTop: top}, 700);
+});
